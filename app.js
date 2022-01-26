d6539334207c15d2f4951fd69e82379f70694986
YenkiGraz <ygraz10@gmail.com>
2022-01-26 19:17:54 +0200

    add some packages and cleanup the code at app.js

--- app.js
+++ app.js
@@ -6,30 +6,17 @@ const containersRouter = require('./src/routes/containers');
 const logger = require("./src/logger/functionLogger");
 const httpLogger = require('./src/logger/httpLogger')
 
-//  TODO - logger
 const app = express();
 const PORT = process.env.PORt || 3000
-
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 async function appGenerator(app) {
     app.use(cors()); // Init cors
     app.use(express.json()); // Init json
     app.use(httpLogger); // Init http logger
     app.use('/api/v1/containers', containersRouter); // Init routes
-
     await connectDb(); // Init db connection
 
     // TODO - error handler
-
 }
 
 appGenerator(app)
